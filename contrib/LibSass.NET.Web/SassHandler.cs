dc676272608aa890a94df0cc08eb53a0aafd8a49
Dave Long <david@itbsoftware.co.za>
2019-05-30 13:06:52 +0200

    Update Handler
    Remove "ignore" of include files, we want the requested file to compile

--- contrib/LibSass.NET.Web/SassHandler.cs
+++ contrib/LibSass.NET.Web/SassHandler.cs
@@ -15,15 +15,9 @@ namespace LibSass.Web
             SassCompiler compiler = new SassCompiler(new SassOptions { InputPath = path });
             SassResult result = compiler.Compile();
 
-            if (!file.Name.StartsWith("_") && string.Equals(file.Extension, ".scss", StringComparison.OrdinalIgnoreCase))
-            {
-                context.Response.ContentType = "text/css";
-                context.Response.Write(result.Output);
-            }
-            else if (file.Name.EndsWith(".css", StringComparison.OrdinalIgnoreCase) && string.Equals(file.Extension, ".map", StringComparison.OrdinalIgnoreCase))
-            {
-                context.Response.Write(result.Output);
-            }
+            context.Response.ContentType = "text/css";
+            context.Response.Write(result.Output);
+
         }
 
         public bool IsReusable { get { return true; } }
